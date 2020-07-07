b5894dc785c2608c6143f561073d6b0249b10d4c
bishopwalker <walkerbishop333@gmail.com>
2020-07-07 07:59:59 -0700

    yup

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -15,7 +15,8 @@ class About extends Component {
             <div className='about'>
                 <p>copyright 2020 </p>
                 <b>wr-2 Bishop Walker </b>
-                <span className='span1' >All Lives Matter</span>....with love!
+                <span className='span1' >All Lives Matter</span>
+                <small>My wife is Russian, my daugher mixed. My in-laws are just as important as my parents....</small>
                 <button className='big-button' onClick={this.goBack} >Go Back</button>
                 <p>{today}</p>
                 
